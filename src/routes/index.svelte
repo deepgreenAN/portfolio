8471216fc63523624f1ed609b4bdd9f5e101e097
deepgreenAN <asami73dgreen63@gmail.com>
2021-11-14 22:43:20 +0900

    add about me

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -20,7 +20,31 @@
     export let profile_data: Profile;
 </script>
 
-<div>
-    {@html profile_data.about_me_desc}
+<div class="w-3/5 mx-auto">
+    <h2 class="pl-3 text-3xl">AboutMe</h2>
+    <div class="bg-gray-300 opacity-60 border-black border-2 border-solid rounded">
+        <div class="p-1"> {@html profile_data.about_me_desc} </div>
+        <hr>
+        <div class="flex flex-row">
+            <div class="w-1/2">
+                <div> フレームワーク・言語 </div>
+                <ul class="break-words">
+                    {#each profile_data.skills as skill}
+                    <li class="ml-4">{skill}</li>
+                    {/each}
+                </ul>
+            </div>
+            <div class="w-1/2">
+                <div> その他のスキル </div>
+                <ul class="break-words">
+                    {#each profile_data.other_skills as other_skill}
+                    <li class="ml-4">{other_skill}</li>
+                    {/each}
+                </ul>
+            </div>
+        </div>
+    </div>
+
 </div>
+
 
